20971e7fd237269494acaa7a0951f8e3f136fe52
kinoshita0623 <kuutarousii@gmail.com>
2021-06-02 16:40:40 +0900

    画像の添付以外の投稿機能を作った

--- app/src/main/java/ie2a_2200078/eventwork05/GalleryNoteEditorActivity.kt
+++ app/src/main/java/ie2a_2200078/eventwork05/GalleryNoteEditorActivity.kt
@@ -2,10 +2,30 @@ package ie2a_2200078.eventwork05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import ie2a_2200078.eventwork05.databinding.ActivityGalleryNoteEditorBinding
+import ie2a_2200078.eventwork05.viewmodels.GalleryPostEditorViewModel
 
 class GalleryNoteEditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gallery_note_editor)
+        val binding = DataBindingUtil.setContentView<ActivityGalleryNoteEditorBinding>(this, R.layout.activity_gallery_note_editor)
+        binding.lifecycleOwner = this
+
+        val viewModel = ViewModelProvider(this, GalleryPostEditorViewModel.Factory(application as MyApp))[GalleryPostEditorViewModel::class.java]
+        binding.galleryPostEditorViewModel = viewModel
+
+        binding.saveFab.setOnClickListener {
+            viewModel.save()
+        }
+
+        viewModel.isSuccess.observe(this){
+            if(it == true) {
+                finish()
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/ie2a_2200078/eventwork05/viewmodels/GalleryPostEditorViewModel.kt
@@ -0,0 +1,65 @@
+package ie2a_2200078.eventwork05.viewmodels
+
+import androidx.lifecycle.*
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ie2a_2200078.eventwork05.MyApp
+import ie2a_2200078.eventwork05.dao.GalleryFileDAO
+import ie2a_2200078.eventwork05.dao.GalleryNoteDAO
+import ie2a_2200078.eventwork05.entities.GalleryNote
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class GalleryPostEditorViewModel(
+    val galleryNoteDAO: GalleryNoteDAO,
+    val galleryFileDAO: GalleryFileDAO
+) : ViewModel(){
+
+    @Suppress("UNCHECKED_CAST")
+    class Factory(
+        private val app: MyApp
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return GalleryPostEditorViewModel(app.database.galleryNoteDAO(), app.database.galleryFileDAO()) as T
+        }
+    }
+
+    val name = MutableLiveData<String>()
+
+
+    val description = MutableLiveData<String>()
+
+
+    private val _saveError = MutableLiveData<Throwable>()
+    val saveError: LiveData<Throwable> = _saveError
+
+    private val _isSuccess = MutableLiveData<Boolean>(false)
+    val isSuccess: LiveData<Boolean> = _isSuccess
+
+    fun save() {
+        val n = name.value
+        val description = this.description.value
+        if(n.isNullOrBlank()) {
+            return
+        }
+        val note = GalleryNote(
+            n,
+            description
+        )
+
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                galleryNoteDAO.insert(note)
+            }.onSuccess {
+                _isSuccess.postValue(true)
+            }.onFailure {
+                _saveError.postValue(it)
+            }
+
+
+        }
+
+
+
+    }
+}
