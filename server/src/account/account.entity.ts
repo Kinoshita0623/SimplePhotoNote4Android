67dd71d300838638640b7be935a5cf8e9915974b
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-22 20:27:58 +0900

    [change] accounts entity

--- server/src/account/account.entity.ts
+++ server/src/account/account.entity.ts
@@ -1,3 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+    name: 'accounts'
+})
 export class Account {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        unique: true
+    })
     username: string;
+
+    @Column()
+    encryptedPassword: string;
+    
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AccountService } from './account/account.service';
 import { AccountModule } from './account/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, AccountModule],
+  imports: [AuthModule, 
+    AccountModule, 
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sqlitedb.db',
+      synchronize: true,
+      entities: ['src/**/*.entity.ts']
+    })
+  ],
   controllers: [AppController],
   providers: [AppService, AccountService],
 })
