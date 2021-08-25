27bd4eda7ffd27aeff45d403fe79ba85da134a42
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-25 11:51:35 +0900

    例外の実装をしました

--- app/src/main/java/ie2a_2200078/eventwork05/client/errors.kt
+++ app/src/main/java/ie2a_2200078/eventwork05/client/errors.kt
@@ -1,6 +1,29 @@
 package ie2a_2200078.eventwork05.client
 
+import retrofit2.Response
+import okhttp3.Response as OkHttpResponse
+
 class UnauthorizedException : IllegalStateException()
 class ClientException : Exception()
 class NotFoundException : NoSuchElementException()
-class ServerInternalException : Exception()
+class InternalServerException : Exception()
+
+fun<T> Response<T>.throwErrors(): Response<T> {
+    when(this.code()) {
+        400 -> throw ClientException()
+        401 -> throw UnauthorizedException()
+        404 -> throw NotFoundException()
+        500 -> throw InternalServerException()
+    }
+    return this
+}
+
+fun OkHttpResponse.throwErrors(): OkHttpResponse {
+    when(this.code) {
+        400 -> throw ClientException()
+        401 -> throw UnauthorizedException()
+        404 -> throw NotFoundException()
+        500 -> throw InternalServerException()
+    }
+    return this
+}
