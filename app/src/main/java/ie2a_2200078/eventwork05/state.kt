73c71fb01c5f8b97a5eec537e94a8efbd370bd57
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-25 11:52:26 +0900

    修正

--- app/src/main/java/ie2a_2200078/eventwork05/state.kt
+++ app/src/main/java/ie2a_2200078/eventwork05/state.kt
@@ -1,9 +1,9 @@
 package ie2a_2200078.eventwork05
 
 sealed interface State<T> {
-    data class Fixed<T>(val content: Content<T>)
-    data class Loading<T>(val content: Content<T>)
-    data class Error<T>(val content: Content<T>)
+    data class Fixed<T>(val content: Content<T>) : State<T>
+    data class Loading<T>(val content: Content<T>) : State<T>
+    data class Error<T>(val content: Content<T>, val error: Throwable) : State<T>
 }
 
 sealed interface Content<T> {
